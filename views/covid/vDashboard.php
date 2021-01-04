04de367617253a7531664426740ec93e3885ab8a
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2021-01-04 14:29:51 +0700

    Create vDashboard.php

---
+++ views/covid/vDashboard.php
@@ -0,0 +1,20 @@
+<body>
+
+  <div id="wrapper">
+
+    <div id="page-wrapper">
+
+      <div class="row">
+        <div class="col-lg-12">
+          <h1>Dashboard <small>Selasa, 10 November 2020</small></h1>
+          <ol class="breadcrumb">
+            <li class="active"><i class="fa fa-dashboard"></i> Dashboard</li>
+          </ol>
+          <?php $this->load->view('templates/welcome') ?>
+        </div>
+
+      </div><!-- /.row -->
+
+    </div><!-- /#page-wrapper -->
+
+    <!--</div> /#wrapper -->
