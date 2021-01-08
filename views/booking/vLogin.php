6b590790c045ad8b84ae73e26c7e146c123d0630
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2021-01-08 13:59:55 +0700

    Add files via upload

--- views/booking/vLogin.php
+++ views/booking/vLogin.php
@@ -12,14 +12,17 @@
 <body>
 
   <div class="login_box">
-    <div align="center"><i class="fa fa-user-circle-o fa-3x"></i></div><br>
-    <?php echo $this->session->flashdata('alert') ?>
-    <form method="post" action="<?php echo base_url('booking/Login/login') ?>" role="form">
-      <input type="hidden" name="id_aplikasi" value="1">
-      <input type="text" name="username" class="login_form" placeholder="Username" required>
-      <input type="password" name="password" class="login_form" placeholder="Password" required>
-      <input type="submit" class="login_submit" name="login" value="Login">
-      <br><br>      
-    </form>
-    <a href="<?php echo base_url() ?>"><i class="fa fa-arrow-left"></i> Back</a>
-  </div>
+    <div align="center">
+      <a href="https://instagram.com/arditriheru">
+        <img class="img-responsive" src="<?php echo base_url('assets/images/logo-rskia-rachmi-akreditasi.jpg') ?>" width="70%" height="auto" alt="Author Ardi Tri Heru Hatmoko / arditriheruh@gmail.com"></a><!-- <i class="fa fa-user-circle-o fa-3x"></i> -->
+      </div><br>
+      <?php echo $this->session->flashdata('alert') ?>
+      <form method="post" action="<?php echo base_url('booking/Login/login') ?>" role="form">
+        <input type="hidden" name="id_aplikasi" value="1">
+        <input type="text" name="username" class="login_form" placeholder="Username" required>
+        <input type="password" name="password" class="login_form" placeholder="Password" required>
+        <input type="submit" class="login_submit" name="login" value="Login">
+        <br><br>      
+      </form>
+      <a href="<?php echo base_url() ?>"><i class="fa fa-arrow-left"></i> Back</a>
+    </div>
