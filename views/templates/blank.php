49b6b8c4558dc0875ccf7b575b50e476eba43d99
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2021-01-27 10:47:29 +0700

    Add files via upload

--- views/templates/blank.php
+++ views/templates/blank.php
@@ -11,10 +11,19 @@
             <li><a href="<?php echo base_url('booking/dataBooking') ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
             <li class="active"><i class="fa fa-plus"></i> <?php echo $title ?></li>
           </ol>
-          
+        </div>
+      </div><!-- /.row -->
+
+      <div class="row">
+        <div class="col-lg-12">
           <?php $this->load->view('templates/welcome') ?>
           <?php echo $this->session->flashdata('alert') ?>
+        </div>
+      </div><!-- /.row -->
 
+      <div>
+        <div>
+          content
         </div>
       </div><!-- /.row -->
 
