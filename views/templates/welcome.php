e810dbd1672bc29ea6c2b0762be02281479f4718
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2021-02-03 11:00:45 +0700

    Add files via upload

--- views/templates/welcome.php
+++ views/templates/welcome.php
@@ -1,4 +1,6 @@
 <div class="alert alert-info alert-dismissable">
-	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
 	<h4>Selamat Datang <strong><?php echo $this->session->userdata('hello') ?></strong>, sistem ini dikembangkan oleh IT RSKIA Rachmi Yogyakarta</h4>
+</div>
+<div class="alert alert-warning alert-dismissable">
+	<h5><strong>Update!</strong> Kini dapat mengedit detail data Tumbung Kembang dan ANC Terpadu</h5>
 </div>
